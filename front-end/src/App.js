44cee414e92de7194b770baf669c8bc88474c38c
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-30 03:23:36 +0530

    chat api creation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import { Button } from '@chakra-ui/react';
+import Homepage from './Pages/Homepage';
+import ChatPage from './Pages/ChatPage';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 function App() {
   return (
     <div className="App">
-     
+      <Route path="/" Component={Homepage} exact></Route>
+      <Route path="/chat/api" Component={ChatPage}></Route>
     </div>
   );
 }
---
+++ front-end/src/Pages/Homepage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Homepage = () => {
+  return <div>Hello I am Home</div>;
+};
+
+export default Homepage;
