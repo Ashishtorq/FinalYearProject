f796e01ade183d4e3d495a0a27e1015f8caf78ab
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-30 00:55:08 +0530

    api testing on postman

--- Backend/server.js
+++ Backend/server.js
@@ -9,5 +9,10 @@ app.get('/',(req,res)=>{
 app.get('/api/chat',(req,res)=>{
     res.send(chats)
 })
+app.get('/api/chat/:id',(req,res)=>{
+    const singleChat = chats.find((c)=>c._id === req.params.id)
+    res.send(singleChat);
+})
 
 app.listen(5500,console.log("Running on port 5500"));
+ 
