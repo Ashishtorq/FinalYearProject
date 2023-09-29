469516cf97a2fc0a739ecf169e1ec7ddce388780
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-30 02:04:56 +0530

    dotenv created

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const chats = require("./data/data");
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.get('/',(req,res)=>{
     res.send("API is running");
 })
 
+
 app.get('/api/chat',(req,res)=>{
-    res.send(chats)
+    res.status(200).send(chats)
 })
+
+
 app.get('/api/chat/:id',(req,res)=>{
     const singleChat = chats.find((c)=>c._id === req.params.id)
     res.send(singleChat);
 })
 
-app.listen(5500,console.log("Running on port 5500"));
+const PORT = process.env.PORT || 5500;
+app.listen(5500,console.log(`Running on Port ${PORT}`));
  
