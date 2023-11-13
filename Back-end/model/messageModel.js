cb282245cd4c1b977eff6155dfaecfabd0e649cd
Ashish Singh <98528335+Ashishtorq@users.noreply.github.com>
2023-11-13 19:20:25 +0530

    Update messageModel.js

--- Back-end/model/messageModel.js
+++ Back-end/model/messageModel.js
@@ -1,3 +1,4 @@
+
 const mongoose = require("mongoose");
 
 const messageSchema = mongoose.Schema(
@@ -12,3 +13,17 @@ const messageSchema = mongoose.Schema(
 
 const Message = mongoose.model("Message", messageSchema);
 module.exports = Message;
+// const mongoose = require("mongoose");
+
+// const messageSchema = mongoose.Schema(
+//   {
+//     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+//     content: { type: String, trim: true },
+//     chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+//     readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+//   },
+//   { timestamps: true }
+// );
+
+// const Message = mongoose.model("Message", messageSchema);
+// module.exports = Message;
